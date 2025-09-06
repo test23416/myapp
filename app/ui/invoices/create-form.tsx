03960c18efa1452961d64803821a450d01f0de2b
test23416 <3225795071@qq.com>
2025-09-06 17:59:08 +0800

    fifth try-2

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -67,7 +67,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 step="0.01"
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-                aria-describedby="amount-error"
+                // aria-describedby="amount-error"
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
@@ -133,6 +133,11 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
         </fieldset>
       </div>
 
+      <div id="form-error" aria-live="polite" aria-atomic="true">
+        {state.message && ( // <- 同样是 React 条件渲染
+          <p className="mt-2 text-sm text-red-500">{state.message}</p>
+        )}
+      </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
           href="/dashboard/invoices"
